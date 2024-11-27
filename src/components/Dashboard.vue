655b09d4b5c0705d8a0924f57046bf214ac9e477
Jorge Ivan Villalba Grandett <j.ivanvillalba@gmail.com>
2024-11-26 22:12:29 -0500

    Se crea componente base

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,17 @@
+<!-- TYPESCRIPT -->
+<script setup lang="ts">
+import { ref } from 'vue'
+
+</script>
+
+<!-- HTML -->
+<template>
+    <div class="main-frame">
+        <h2>Este es mi dashboard</h2>
+    </div>
+</template>
+
+
+<!-- CSS -->
+<style scoped>
+</style>
